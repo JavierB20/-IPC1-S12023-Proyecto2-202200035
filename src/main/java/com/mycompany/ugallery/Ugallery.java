30384fed696c0ad9fb896c96bb5c30c369768941
JavierB20 <josejavierb10@gmail.com>
2023-04-23 19:22:07 -0600

    Se agrega la interfaz grafica

---
+++ src/main/java/com/mycompany/ugallery/Ugallery.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ugallery;
+
+/**
+ *
+ * @author Javier
+ */
+public class Ugallery {
+
+    public static void main(String[] args) {
+        GUI.VentanaPrincipal ventana = new GUI.VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
